0452c73409413268f30c5ab4ba2d661af171802e
Pablo Mendoza <soypalilu@gmail.com>
2022-11-20 19:26:26 -0300

    Commit inicial, TPE5 vacio.

---
+++ src/test/java/com/heroku/tpe5/Tpe5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.tpe5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpe5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
